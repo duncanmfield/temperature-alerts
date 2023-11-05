f6bfdbd5c01c32989e7ebfccf06337901c1f06f8
Duncan Field <duncanmfield@gmail.com>
2023-11-05 15:30:02 +0000

    Add test database to allow context to load

--- alert-monitoring-service/src/test/java/com/github/duncanmfield/alertmonitoringservice/integration/AlertMonitoringServiceApplicationTest.java
+++ alert-monitoring-service/src/test/java/com/github/duncanmfield/alertmonitoringservice/integration/AlertMonitoringServiceApplicationTest.java
@@ -1,11 +1,14 @@
 package com.github.duncanmfield.alertmonitoringservice.integration;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.jdbc.EmbeddedDatabaseConnection;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.kafka.annotation.EnableKafka;
 import org.springframework.kafka.test.context.EmbeddedKafka;
 
 @EnableKafka
+@AutoConfigureTestDatabase(connection = EmbeddedDatabaseConnection.H2)
 @SpringBootTest
 @EmbeddedKafka(
         partitions = 1,
